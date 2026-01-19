357007e5eea3deef8a754324244cb0839b1b5e4d
Nejat Alper Satiroglu <n.satiroglu265@mybvc.ca>
2026-01-19 01:27:32 -0700

    added script and object
    Created the input script and  a game object

---
+++ Assets/Scripts/InputTest.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputTest : MonoBehaviour
+{
+    private PlayerInputActions _testActions;
+
+    void Awake()
+    {
+        _testActions = new PlayerInputActions();
+        _testActions.Enable();
+    }
+
+    void OnEnable()
+    {
+        _testActions.Player.Jump.performed += Jump;
+    }
+
+    void OnDisable()
+    {
+        _testActions.Player.Jump.performed -= Jump;
+    }
+
+    void Jump(InputAction.CallbackContext context)
+    {
+        Debug.Log("Jump");
+    }
+}
